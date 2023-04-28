a325aca43d86393342301db40a09b4be9072ffbc
Tushar patel <93108145+Tusharp21@users.noreply.github.com>
2023-04-28 13:21:36 +0530

    first commit

---
+++ unsplash/pipelines.py
@@ -0,0 +1,26 @@
+# # Define your item pipelines here
+# #
+# # Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# # useful for handling different item types with a single interface
+# from itemadapter import ItemAdapter
+
+
+# class UnsplashPipeline:
+#     def process_item(self, item, spider):
+#         return item
+
+
+
+from scrapy.pipelines.images import ImagesPipeline
+from slugify import slugify
+
+
+class CustomImagePipeline(ImagesPipeline):
+
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        file_name = slugify(item['title'],max_length=200)
+        return f"full/{file_name}.jpg"
---
+++ unsplash/spiders/image_spider.py
@@ -0,0 +1,31 @@
+import scrapy 
+from scrapy_selenium import SeleniumRequest
+
+from ..items import UnsplashItem
+
+class ImageSpider(scrapy.Spider):
+    name = 'img_spider'
+    
+    
+    def start_requests(self):
+        category = input("Enter image Category: ")
+        url = 'https://unsplash.com/s/photos/'+category
+        yield SeleniumRequest(url=url, callback=self.parse ,wait_time=10)
+
+    def parse(self,response):
+        row_img_url = response.xpath('//div/img/@src').getall()
+        title = response.xpath('//div/a/@title').getall()
+
+        # print(title)
+        # if title is not None:
+        #     img_title.append(title)
+        # else:
+        #     img_title.append(num)
+
+        clean_url = []
+        for img_url in row_img_url:
+            clean_url.append(response.urljoin(img_url))
+
+        yield{
+            'image_urls': clean_url
+        }
